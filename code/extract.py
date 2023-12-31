43934421568b736b59ec749836d3802fc0118ef3
tierrif <tierriff@gmail.com>
2023-12-31 15:00:03 +0000

    Minor refactorings.

--- code/extract.py
+++ code/extract.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 import os
 
-#Transfrom a csv file to array
+# Transfrom a csv file to array
 def parseCsvToArray(filename):
   reader = csv.reader(
       open(filename, 'r', encoding='utf-8'))
@@ -25,11 +25,10 @@ def parseCsvToArray(filename):
 
   return data
 
-
-#Primary data
+# Primary data
 videos = parseCsvToArray('US_youtube_trending_data.csv')
 
-#Limit data to 4 years
+# Limit data to 4 years
 years = []
 for i in range(len(videos)):
   year = videos[i]['publishedAt']
@@ -37,24 +36,19 @@ for i in range(len(videos)):
   if sub not in years:
     years.append(sub)
 
-
 channels = parseCsvToArray('most_subscribed_youtube_channels.csv')
 
-#Filter primary data for only the most subscribed channels
+# Filter primary data for only the most subscribed channels
 filteredVideos = []
 for i in range(len(channels)):
   channelVideos = list(filter(lambda c: c['channelTitle'] == channels[i]['Youtuber'], videos))
   filteredVideos = filteredVideos + channelVideos
 
-
-
 categories = json.load(open('US_category_id.json'))['items']
 
-
-#Add category title 
+# Add category title 
 for i in range(len(filteredVideos)):
-
-  #Convert dates for use on orange 
+  # Convert dates for use on orange 
   publishedString = datetime.strptime(filteredVideos[i]['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
   filteredVideos[i]['publishedAt'] = str(publishedString.strftime("%Y-%m-%d %H:%M:%S"))
 
@@ -62,22 +56,13 @@ for i in range(len(filteredVideos)):
   filteredVideos[i]['trending_date'] = str(trendingString.strftime("%Y-%m-%d %H:%M:%S"))
 
   for c in categories:
-    if(c['id'] == filteredVideos[i]['categoryId']):
+    if c['id'] == filteredVideos[i]['categoryId']:
       filteredVideos[i]['category'] = c['snippet']['title']
       break
 
-
-
-#Create csv
+# Create CSV
 os.remove('out.csv')
 
 jsonStr = json.dumps(filteredVideos)
 df = pd.read_json(jsonStr)
 df.to_csv('out.csv')
-
-#Create json 
-
-#with open('out.json', 'w') as f:
-#  json.dump(filteredVideos, f)
-
-print('success')
